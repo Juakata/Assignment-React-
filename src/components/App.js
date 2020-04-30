cdfa0ed3b7d63414a44221c72a4a34eec3f052a6
Juakata <juakata07@gmail.com>
2020-04-29 20:13:20 -0600

    create initial structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <header className="App-header">
         <p>React App </p>
       </header>
